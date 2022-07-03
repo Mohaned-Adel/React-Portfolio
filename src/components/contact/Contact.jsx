a7fef962d5a2dcf96075b31d48aefdaabaa3b837
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-07-03 23:41:29 +0200

    added: contact and footer section component with style

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,90 @@
-import React from 'react'
-import './Contact.css'
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_2mskbfd",
+        "template_9zycvyo",
+        form.current,
+        "0GJN0GcskqD8CswDl"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+      e.target.reset()
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>mohaned.adel1996@gmail.com</h5>
+            <a href="mailto:mohaned.adel1996@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsMessenger className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Mohaned Adel</h5>
+            <a href="https://m.me/mohaned.adel.98" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+201204155977</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=201204155977"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="16"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
